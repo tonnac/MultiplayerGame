a80df8d3c19c0fec9888733723435c2ea7abe52c
tonnac35 <tonnac35@gmail.com>
2019-09-24 22:59:22 +0900

    input

---
+++ Source/DxInput/Engine.h
@@ -0,0 +1,39 @@
+#pragma once
+
+class Timing;
+struct PermanentText;
+
+class Engine : public Device
+{
+	using Super = Device;
+
+public:
+	virtual ~Engine();						
+
+public:
+	virtual void Initialize() override;
+
+protected:
+	virtual void OnResize() override;
+
+private:
+	virtual void GameRun()override;
+	virtual void Update(float DeltaTimes);
+	virtual void GameDraw();
+
+	void ClearBuffer();
+	void Draw();
+
+	void CalculateFrameStats();
+
+private:
+	shared_ptr<Timing> mTimer;
+	static constexpr DirectX::XMFLOAT4 mBackColor = Colors::LightBlue;
+
+	friend class Direct2D;
+
+	SINGLETON(Engine)
+};
+
+#define ENGINE Engine::sInstance
+using EnginePtr = unique_ptr<Engine>;
---
+++ Source/DxInput/Main.cpp
@@ -0,0 +1,18 @@
+#include "EnginePCH.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevInstance, _In_ LPSTR cmdLine, _In_ int showCmd)
+{
+	try
+	{
+		ENGINE->StaticInit();
+		ENGINE->SetHInstance(hInstance);
+
+		ENGINE->Initialize();
+		return ENGINE->Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), TEXT("HR Failed"), MB_OK);
+		return 0;
+	}
+}
